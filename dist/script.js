e4f478b06004580b4b28068c242b3b9b1826d06f
Jimmy <jimmy.ganci@gmail.com>
2021-12-16 17:27:07 +0100

    modifed gitignore

--- dist/script.js
+++ dist/script.js
@@ -7,13 +7,13 @@ var setupRoutes = require("./controllers/index").setupRoutes;
 var port = process.env.PORT || 8000;
 app.use(express.json());
 var corsOptions = {
-    origin: true,
-    credentials: true,
-    optionSuccessStatus: 200
+  origin: true,
+  credentials: true,
+  optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 setupRoutes(app);
 app.listen(port, function () {
-    console.log("Server listening on port ".concat(port));
+  console.log("Server listening on port ".concat(port));
 });
 //# sourceMappingURL=script.js.map
